367269c461dd7aa367654b5fac0c511bc0373387
ZhengpeiMo <mo1262510125@163.com>
2018-03-19 22:04:42 +0800

    添加opc client

---
+++ OPC demo/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace OPCClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.connect = new System.Windows.Forms.Button();
+            this.cmbServerName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TagValue = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Status = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.Status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(7, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 14);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.connect);
+            this.groupBox1.Controls.Add(this.cmbServerName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "连接OPC服务器";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 25);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "获取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // connect
+            // 
+            this.connect.Enabled = false;
+            this.connect.Location = new System.Drawing.Point(74, 67);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(49, 25);
+            this.connect.TabIndex = 3;
+            this.connect.Text = "连接";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // cmbServerName
+            // 
+            this.cmbServerName.FormattingEnabled = true;
+            this.cmbServerName.Location = new System.Drawing.Point(6, 39);
+            this.cmbServerName.Name = "cmbServerName";
+            this.cmbServerName.Size = new System.Drawing.Size(185, 22);
+            this.cmbServerName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "服务器：";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(207, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(265, 382);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TagValue);
+            this.groupBox2.Location = new System.Drawing.Point(4, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(196, 220);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "当前值";
+            // 
+            // TagValue
+            // 
+            this.TagValue.Location = new System.Drawing.Point(12, 17);
+            this.TagValue.Multiline = true;
+            this.TagValue.Name = "TagValue";
+            this.TagValue.ReadOnly = true;
+            this.TagValue.Size = new System.Drawing.Size(170, 197);
+            this.TagValue.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnWrite);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Location = new System.Drawing.Point(4, 336);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(196, 55);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "写入值";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Enabled = false;
+            this.btnWrite.Location = new System.Drawing.Point(118, 16);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(64, 31);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "写入";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Status
+            // 
+            this.Status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.Status.Location = new System.Drawing.Point(0, 394);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(474, 22);
+            this.Status.TabIndex = 5;
+            this.Status.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(459, 17);
+            this.toolStripStatusLabel2.Spring = true;
+            this.toolStripStatusLabel2.Text = "河海大学物联网工程学院";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 416);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "OPCClient";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.Status.ResumeLayout(false);
+            this.Status.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.ComboBox cmbServerName;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TagValue;
+        private System.Windows.Forms.StatusStrip Status;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OPC demo/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections;
+using OPCAutomation;
+
+namespace OPCClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// 定义变量
+        /// </summary>
+        OPCServer KepServer;
+        OPCGroups KepGroups;
+        OPCGroup KepGroup;
+        OPCBrowser oPCBrowser;
+        OPCItems KepItems;
+        OPCItem KepItem;
+
+        int itmHandleClient = 0;
+        int itmHandleServer = 0;
+        
+        /// <summary>
+        /// 获取本地的OPC Server
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                KepServer = new OPCServer();
+                object serverList = KepServer.GetOPCServers("");
+                cmbServerName.Items.Clear();
+                foreach (string turn in (Array)serverList)
+                {
+                    cmbServerName.Items.Add(turn);
+                }
+
+                cmbServerName.SelectedIndex = 0;
+                connect.Enabled = true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("枚举本地OPC服务器出错：" + err.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+         /// <summary>
+        /// 列出OPC服务器中所有节点
+        /// </summary>
+        private void RecurBrowse(OPCBrowser oPCBrowser)
+        {
+            //展开分支
+            oPCBrowser.ShowBranches();
+            //展开叶子
+            oPCBrowser.ShowLeafs(true);
+            foreach (object turn in oPCBrowser)
+            {
+                //if (string.Compare(turn.ToString(),"Tags")==0)//
+                if((turn.ToString().IndexOf("Tags"))>-1)
+                {
+                    listBox1.Items.Add(turn.ToString());
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// 建立连接按钮
+        /// </summary>
+        private void connect_Click(object sender, EventArgs e)  
+        {
+
+            try
+            {
+                KepServer.Connect(cmbServerName.Text);
+                KepGroups = KepServer.OPCGroups;
+                KepServer.OPCGroups.DefaultGroupIsActive = true;
+                KepServer.OPCGroups.DefaultGroupDeadband = 0;
+                KepServer.OPCGroups.DefaultGroupUpdateRate = 250;
+                KepGroup = KepGroups.Add("OPCDOTNETGROUP");
+
+                KepGroup.IsActive = true;
+                KepGroup.IsSubscribed = true;
+
+                oPCBrowser = KepServer.CreateBrowser();
+                oPCBrowser.ShowBranches();
+                oPCBrowser.ShowLeafs(true);
+
+                listBox1.Items.Clear();
+                RecurBrowse(oPCBrowser);
+
+                KepGroup.DataChange += new DIOPCGroupEvent_DataChangeEventHandler(KepGroup_DataChange);
+                //KepGroup.AsyncWriteComplete += new DIOPCGroupEvent_AsyncWriteCompleteEventHandler(KepGroup_AsyncWriteComplete);
+                KepItems = KepGroup.OPCItems;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("连接服务器出现错误：" + err.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+        }
+
+        /// <summary>
+        /// 每当项数据有变化时执行的事件
+        /// </summary>
+        /// <param name="TransactionID">处理ID</param>
+        /// <param name="NumItems">项个数</param>
+        /// <param name="ClientHandles">项客户端句柄</param>
+        /// <param name="ItemValues">TAG值</param>
+        void KepGroup_DataChange(int TransactionID, int NumItems, ref Array ClientHandles, ref Array ItemValues, ref Array Qualities, ref Array TimeStamps)
+        {
+            for (int i = 1; i <= NumItems; i++)
+            {
+                this.TagValue.Text = ItemValues.GetValue(i).ToString();
+            }
+        }
+
+        /// <summary>
+        /// 【按钮】写入
+        /// </summary>
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            OPCItem bItem = KepItems.GetOPCItem(itmHandleServer);
+            int[] temp = new int[2] { 0, bItem.ServerHandle };
+            Array serverHandles = (Array)temp;
+            object[] valueTemp = new object[2] { "", textBox1.Text };
+            Array values = (Array)valueTemp;
+            Array Errors;
+            int cancelID;
+            KepGroup.AsyncWrite(1, ref serverHandles, ref values, out Errors, 2009, out cancelID);
+            GC.Collect();
+        }
+
+        /// <summary>
+        /// 关闭窗体时处理的事情
+        /// </summary>
+        private void MainFrom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (KepGroup != null)
+            {
+                KepGroup.DataChange -= new DIOPCGroupEvent_DataChangeEventHandler(KepGroup_DataChange);
+            }
+
+            if (KepServer != null)
+            {
+                KepServer.Disconnect();
+                KepServer = null;
+            }
+        }
+
+
+        //选中事件
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (itmHandleClient != 0)
+                {
+                    this.TagValue.Text = "";
+
+                    Array Errors;
+                    OPCItem bItem = KepItems.GetOPCItem(itmHandleServer);
+                    //注：OPC中以1为数组的基数
+                    int[] temp = new int[2] { 0, bItem.ServerHandle };
+                    Array serverHandle = (Array)temp;
+                    //移除上一次选择的项
+                    KepItems.Remove(KepItems.Count, ref serverHandle, out Errors);
+                }
+                itmHandleClient = 1;
+                KepItem = KepItems.AddItem(listBox1.SelectedItem.ToString(), itmHandleClient);
+                itmHandleServer = KepItem.ServerHandle;
+                TagValue.Text = KepItem.ToString();
+            }
+            catch (Exception err)
+            {
+                //没有任何权限的项，都是OPC服务器保留的系统项，此处可不做处理。
+                itmHandleClient = 0;
+                TagValue.Text = "Error ox";
+                MessageBox.Show("此项为系统保留项:" + err.Message, "提示信息");
+            }
+        }
+
+        //退出按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (KepGroup != null)
+            {
+                KepGroup.DataChange -= new DIOPCGroupEvent_DataChangeEventHandler(KepGroup_DataChange);
+            }
+
+            if (KepServer != null)
+            {
+                KepServer.Disconnect();
+                KepServer = null;
+            }
+            this.Close();
+        }
+
+    }
+}
